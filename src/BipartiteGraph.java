3b9521980020b5c79200cef300dca071e6002ea3
Yucheng Tu <yuchengtu.uwm@gmail.com>
2014-03-20 16:14:08 -0500

    test completed up to most shared artist list

--- src/BipartiteGraph.java
+++ src/BipartiteGraph.java
@@ -8,13 +8,13 @@ public class BipartiteGraph {
 	public BipartiteGraph(){
 		BGMap = new HashMap<Integer, ArtistNode>();
 	}
-	
+
 	public void addArtist(String artistName, int listenerAge){
 		ArtistNode artist = new ArtistNode(artistName);
 		artist.getAgeGroupByAge(listenerAge).incNumListeners();
 		BGMap.put(artist.getKey(), artist);
 	}
-	
+
 	public ArtistNode lookUpArtist(String name){
 		if(BGMap.containsKey(name.hashCode())){
 			return BGMap.get(name.hashCode());
@@ -22,7 +22,7 @@ public class BipartiteGraph {
 			return null;
 		}
 	}
-	
+
 	public int getNumListenersByArtistAge(String name, int age){
 		ArtistNode artist = lookUpArtist(name);
 		if(artist == null){
@@ -45,12 +45,12 @@ public class BipartiteGraph {
 		}
 		int j = 0;
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    listenerArr[j] = currArtist.getAgeGroupByAge(age).getNumListners();
-		    j++;
+			ArtistNode currArtist = entry.getValue();
+			listenerArr[j] = currArtist.getAgeGroupByAge(age).getNumListners();
+			j++;
 		}
-		
-		for(int p = 0; p < listenerArr.length; p++){
+
+		for(int p = 0; p < listenerArr.length-1; p++){
 			for(int q = p + 1; q < listenerArr.length; q++){
 				//inc the smaller one's ranking
 				if(listenerArr[p] < listenerArr[q]){
@@ -63,35 +63,45 @@ public class BipartiteGraph {
 		//update rankings
 		int k = 0;
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    currArtist.getAgeGroupByAge(age).setRanking(rankings[k]);
-		    k++;
+			ArtistNode currArtist = entry.getValue();
+			currArtist.getAgeGroupByAge(age).setRanking(rankings[k]);
+			k++;
 		}
 	}
 	public void rankArtistsForAllAges(){
 		int startAge = ArtistNode.getStartAge();
 		int ageCoverage = ArtistNode.getStepAge();
 		int currStartAge = startAge;
-		for(int i = 0; i < ArtistNode.getNumAgeGroups(); i++){
+		rankArtistsByAge(currStartAge);
+		for(int i = 1; i < ArtistNode.getNumAgeGroups(); i++){
+
+			currStartAge += ageCoverage;
 			rankArtistsByAge(currStartAge);
-			currStartAge += ageCoverage * i;
 		}
 	}
-	
+	/**
+	 * The most shared artist given two ages is the one who has the smallest
+	 * ranking difference between these two age groups
+	 * @param age1
+	 * @param age2
+	 * @return
+	 */
 	public ArtistNode findMostSharedArtist(int age1, int age2){
 		int minRankingDiff = -1;//sentinal value
 		ArtistNode mostSharedArtist = null;
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		//traverse through all artists
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
-		    int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
-		    int currDiff = Math.abs(rk1 - rk2);
-		    if((minRankingDiff != -1 && currDiff < minRankingDiff) || minRankingDiff == -1){
-		    	minRankingDiff = currDiff;
-		    	mostSharedArtist = currArtist;
-		    }
+			ArtistNode currArtist = entry.getValue();
+			int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
+			int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
+			int currDiff = Math.abs(rk1 - rk2);
+			System.out.println(currArtist.getArtistName() + " " + rk1+", "+rk2);
+			if((minRankingDiff != -1 && currDiff < minRankingDiff) || minRankingDiff == -1){
+				minRankingDiff = currDiff;
+				//System.out.println(rk1+", "+rk2);
+				mostSharedArtist = currArtist;
+			}
 		}
 		return mostSharedArtist;
 	}
@@ -100,22 +110,31 @@ public class BipartiteGraph {
 		List<ArtistNode> sArtists = new LinkedList<ArtistNode>();
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
-		    int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
-		    int currDiff = Math.abs(rk1 - rk2);
-		    if(rkDiff.isEmpty()){
-		    	rkDiff.add(currDiff);
-		    	sArtists.add(currArtist);
-		    }
-		    boolean replaced = false;
-		    for(int i = 1; i < rkDiff.size() && !replaced; i++){
-		    	 if(currDiff <= rkDiff.get(i)){
-		    		 rkDiff.add(i, currDiff);
-		    		 sArtists.add(i, currArtist);
-		    		 replaced = true;
-		    	 }
-		    }
+			ArtistNode currArtist = entry.getValue();
+			int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
+			int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
+			int currDiff = Math.abs(rk1 - rk2);
+			//rkDiff.add(currDiff);
+			if(rkDiff.isEmpty()){
+				rkDiff.add(currDiff);		
+				sArtists.add(currArtist);
+			}else{
+				boolean inserted = false;
+				//System.out.println(rkDiff.size());
+				int currRkDiffSize = rkDiff.size();
+				for(int i = 0; i < currRkDiffSize && !inserted; i++){
+					if(currDiff <= rkDiff.get(i)){
+						rkDiff.add(i, currDiff);
+						sArtists.add(i, currArtist);
+						inserted = true;
+						//System.out.println(rkDiff.size());
+					}
+				}
+				if(!inserted){
+					rkDiff.add(currDiff);
+					sArtists.add(currArtist);
+				}
+			}
 		}
 		return sArtists;
 	}
@@ -125,39 +144,39 @@ public class BipartiteGraph {
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		//traverse through all artists
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
-		    int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
-		    int currDiff = Math.abs(rk1 - rk2);
-		    if(currDiff > maxRkDiff){
-		    	maxRkDiff = currDiff;
-		    	mostDistArtist = currArtist;
-		    }
+			ArtistNode currArtist = entry.getValue();
+			int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
+			int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
+			int currDiff = Math.abs(rk1 - rk2);
+			if(currDiff > maxRkDiff){
+				maxRkDiff = currDiff;
+				mostDistArtist = currArtist;
+			}
 		}
 		return mostDistArtist;
 	}
 	public List<ArtistNode> findMostDArtists(int age1, int age2){
 		List<ArtistNode> dArtists = new LinkedList<ArtistNode>();
 		List<Integer> rkDiff = new LinkedList<Integer>();
-		
+
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
-		    int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
-		    int currDiff = Math.abs(rk1 - rk2);
-		    if(rkDiff.isEmpty()){
-		    	rkDiff.add(currDiff);
-		    	dArtists.add(currArtist);
-		    }
-		    boolean replaced = false;
-		    for(int i = 1; i < rkDiff.size() && !replaced; i++){
-		    	 if(currDiff >= rkDiff.get(i)){
-		    		 rkDiff.add(i, currDiff);
-		    		 dArtists.add(i, currArtist);
-		    		 replaced = true;
-		    	 }
-		    }
+			ArtistNode currArtist = entry.getValue();
+			int rk1 = currArtist.getAgeGroupByAge(age1).getRanking();
+			int rk2 = currArtist.getAgeGroupByAge(age2).getRanking();
+			int currDiff = Math.abs(rk1 - rk2);
+			if(rkDiff.isEmpty()){
+				rkDiff.add(currDiff);
+				dArtists.add(currArtist);
+			}
+			boolean replaced = false;
+			for(int i = 1; i < rkDiff.size() && !replaced; i++){
+				if(currDiff >= rkDiff.get(i)){
+					rkDiff.add(i, currDiff);
+					dArtists.add(i, currArtist);
+					replaced = true;
+				}
+			}
 		}
 		return dArtists;
 	}
@@ -166,13 +185,13 @@ public class BipartiteGraph {
 		ArtistNode mostPopularArtist = null;
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int currRk = currArtist.getAgeGroupByAge(age).getRanking();
-		    if((minRk != -1 && currRk < minRk) || minRk == -1){
-		    	minRk = currRk;
-		    	mostPopularArtist = currArtist;
-		    }
-		    
+			ArtistNode currArtist = entry.getValue();
+			int currRk = currArtist.getAgeGroupByAge(age).getRanking();
+			if((minRk != -1 && currRk < minRk) || minRk == -1){
+				minRk = currRk;
+				mostPopularArtist = currArtist;
+			}
+
 		}
 		return mostPopularArtist;
 	}
@@ -195,46 +214,46 @@ public class BipartiteGraph {
 		}
 		return worstRk;	
 	}
-	
+
 	public ArtistNode findMostHomogeneousArtist(){
 		ArtistNode mostHArtist = null;
 		int minWorstRk = -1;
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int currWorstRk = findWorstRankingByArtist(currArtist);
-		    if((minWorstRk != -1 && minWorstRk < currWorstRk) || minWorstRk == -1){
-		    	minWorstRk = currWorstRk;
-		    	mostHArtist = currArtist;
-		    }
+			ArtistNode currArtist = entry.getValue();
+			int currWorstRk = findWorstRankingByArtist(currArtist);
+			if((minWorstRk != -1 && minWorstRk < currWorstRk) || minWorstRk == -1){
+				minWorstRk = currWorstRk;
+				mostHArtist = currArtist;
+			}
 		}
 		return mostHArtist;	
 	}
-	
+
 	public List<ArtistNode> findMostHArtists(int numArtists){
 		Set<Map.Entry<Integer, ArtistNode>> allArtists = BGMap.entrySet();
 		List<ArtistNode> hArtists = new LinkedList<ArtistNode>();
 		List<Integer> minWRks = new LinkedList<Integer>();
-		
+
 		for (Map.Entry<Integer, ArtistNode> entry : allArtists) {	    	   
-		    ArtistNode currArtist = entry.getValue();
-		    int currWorstRk = findWorstRankingByArtist(currArtist);
-		    if(minWRks.isEmpty()){
-		    	minWRks.add(currWorstRk);
-		    	hArtists.add(currArtist);
-		    }
-		    boolean replaced = false;
-		    for(int i = 1; i < minWRks.size() && !replaced; i++){
-		    	 if(currWorstRk <= minWRks.get(i)){
-		    		 minWRks.add(i, currWorstRk);
-		    		 hArtists.add(i, currArtist);
-		    		 replaced = true;
-		    	 }
-		    }
+			ArtistNode currArtist = entry.getValue();
+			int currWorstRk = findWorstRankingByArtist(currArtist);
+			if(minWRks.isEmpty()){
+				minWRks.add(currWorstRk);
+				hArtists.add(currArtist);
+			}
+			boolean replaced = false;
+			for(int i = 1; i < minWRks.size() && !replaced; i++){
+				if(currWorstRk <= minWRks.get(i)){
+					minWRks.add(i, currWorstRk);
+					hArtists.add(i, currArtist);
+					replaced = true;
+				}
+			}
 		}
 		return hArtists;
 	}
-	
+
 	public HashMap<Integer, ArtistNode> getBGHashMap(){
 		return BGMap;
 	}
--- src/bgMain.java
+++ src/bgMain.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 
 public class bgMain {
 
@@ -6,23 +8,51 @@ public class bgMain {
 		BGTester.testAddArtist("Kevin Alford");
 		BipartiteGraph bg = BGTester.testBGConstrut();
 		bg.addArtist("YUCHENG", 17);
-		System.out.println(bg.getNumListenersByArtistAge("YUCHENG", 18));
+		
 		ArtistNode yucheng = bg.getBGHashMap().get(("YUCHENG").hashCode());
 		yucheng.getAgeGroupByAge(17).incNumListeners();
 		yucheng.getAgeGroupByAge(17).incNumListeners();
-		yucheng.getAgeGroupByAge(17).incNumListeners();
-		yucheng.getAgeGroupByAge(17).incNumListeners();
+		
+		yucheng.getAgeGroupByAge(30).incNumListeners();
+		yucheng.getAgeGroupByAge(34).incNumListeners();
+		yucheng.getAgeGroupByAge(30).incNumListeners();
+		yucheng.getAgeGroupByAge(34).incNumListeners();
+		yucheng.getAgeGroupByAge(30).incNumListeners();
+		
+		yucheng.getAgeGroupByAge(22).incNumListeners();
+		yucheng.getAgeGroupByAge(22).incNumListeners();
+		yucheng.getAgeGroupByAge(22).incNumListeners();
+		yucheng.getAgeGroupByAge(22).incNumListeners();
 		System.out.println(bg.getNumListenersByArtistAge("YUCHENG", 18));
+		System.out.println(bg.getNumListenersByArtistAge("YUCHENG", 23));
+		System.out.println(bg.getNumListenersByArtistAge("YUCHENG", 32));
 		
 		bg.addArtist("Kevin", 16);
+		bg.getBGHashMap().get(("Kevin").hashCode()).getAgeGroupByAge(33).incNumListeners();
+		bg.getBGHashMap().get(("Kevin").hashCode()).getAgeGroupByAge(31).incNumListeners();
+		bg.getBGHashMap().get(("Kevin").hashCode()).getAgeGroupByAge(32).incNumListeners();
 		
 		
 		bg.addArtist("Adam", 17);
 		bg.getBGHashMap().get(("Adam").hashCode()).getAgeGroupByAge(16).incNumListeners();
 		bg.getBGHashMap().get(("Adam").hashCode()).getAgeGroupByAge(16).incNumListeners();
 		
-		bg.rankArtistsByAge(16);
-		System.out.println(bg.findMostPopularArtistByAge(16).getArtistName());
+		//bg.rankArtistsByAge(16);
+		//System.out.println(bg.findMostPopularArtistByAge(16).getArtistName());
+		//bg.rankArtistsByAge(30);
+		//System.out.println(bg.findMostPopularArtistByAge(31).getArtistName());
+		
+		bg.rankArtistsForAllAges();
+		//System.out.println(bg.findMostPopularArtistByAge(31).getArtistName());
+		//System.out.println(bg.findMostPopularArtistByAge(19).getArtistName());
+		
+		System.out.println(bg.findMostSharedArtist(18, 32).getArtistName());
+		System.out.println(bg.findMostDistinctiveArtist(18, 32).getArtistName());
+		
+		List<ArtistNode> mostS = bg.findMostSArtists(18, 32);
+		for(int i = 0; i < mostS.size(); i++){
+			System.out.print(mostS.get(i).getArtistName() +", ");
+		}
 	}
 
 }
